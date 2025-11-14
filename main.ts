711dcc4ea272233bd2b0b5b5b557748362b57e8d
jayjaybean24 <elwood6490@sps81.org>
2025-11-14 14:17:23 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,6 +13,12 @@ player.onChat("dig", function () {
 player.onChat("jump", function () {
     agent.move(UP, 1)
 })
+player.onArrowShot(function () {
+    space.createSun(pos(ARROW_PROJECTILE_MOB, ARROW_PROJECTILE_MOB, ARROW_PROJECTILE_MOB))
+})
+events.onEndOfDay(function () {
+	
+})
 player.onChat("right", function () {
     agent.move(RIGHT, 1)
 })
@@ -39,6 +45,19 @@ player.onChat("god is here", function () {
 player.onChat("down", function () {
     agent.move(BACK, 1)
 })
+player.onChat("go to heaven my child", function () {
+    mobs.applyEffect(LEVITATION, mobs.target(LOCAL_PLAYER), 600, 255)
+})
+player.onChat("old man", function () {
+    mobs.applyEffect(SLOWNESS, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(MINING_FATIGUE, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(BLINDNESS, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(POISON, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(HUNGER, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(WEAKNESS, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(NAUSEA, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 600, 255)
+})
 player.onChat("turn right", function () {
     agent.turn(RIGHT_TURN)
 })
@@ -54,4 +73,17 @@ player.onChat("attack", function () {
 })
 player.onChat("turn left", function () {
     agent.turn(LEFT_TURN)
+})
+player.onChat("i am death", function () {
+    mobs.applyEffect(STRENGTH, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(RESISTANCE, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(FIRE_RESISTANCE, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(WATER_BREATHING, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(ABSORPTION, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(HEALTH_BOOST, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(SPEED, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(HASTE, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(INVISIBILITY, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(NIGHT_VISION, mobs.target(LOCAL_PLAYER), 600, 255)
+    mobs.applyEffect(REGENERATION, mobs.target(LOCAL_PLAYER), 600, 255)
 })
